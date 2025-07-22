0853af12e4286b8e43683455d34bcb3c77cabbda
Novembermonkey <djodio2023@gmail.com>
2025-07-22 13:23:20 +0500

    svdfb

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,8 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['https://railwayytestdeploy.up.railway.app/', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://railwayytestdeploy.up.railway.app/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['railwayytestdeploy.up.railway.app', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://railwayytestdeploy.up.railway.app', 'http://localhost', 'http://127.0.0.1']
 
 # Application definition
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,12 +4,12 @@ from ecommerce import views
 app_name = 'ecommerce'
 
 urlpatterns = [
-        path('', views.Index.as_view(), name='index'),
-        path('product-list/', views.ProductList.as_view(), name='product_list'),
-        path('category/<slug:category_slug>', views.Index.as_view(), name = 'products_by_category'),
-        path('product/detail/<int:pk>', views.ProductDetail.as_view(), name = 'product_detail'),
-        path('product/detail/<int:pk>/comment/create', views.CommentCreate.as_view(), name = 'create_comment'),
+        path('ecommerce/', views.Index.as_view(), name='index'),
+        path('ecommerce/product-list/', views.ProductList.as_view(), name='product_list'),
+        path('ecommerce/category/<slug:category_slug>', views.Index.as_view(), name = 'products_by_category'),
+        path('ecommerce/product/detail/<int:pk>', views.ProductDetail.as_view(), name = 'product_detail'),
+        path('ecommerce/product/detail/<int:pk>/comment/create', views.CommentCreate.as_view(), name = 'create_comment'),
 
         #email
-        path('send-email/', views.send_email, name = 'send_email'),
+        path('ecommerce/send-email/', views.send_email, name = 'send_email'),
 ]
